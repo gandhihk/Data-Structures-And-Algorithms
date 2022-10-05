9542d6f30699c06e50dc4c9f69996fbdfdb49f74
Harsh Gandhi <harsh31.gandhi204@gmail.com>
2022-10-05 18:40:54 +0530

    Update MinNoOfSwapsToSortArray.java

--- DataStructures/Arrays/MinNoOfSwapsToSortArray.java
+++ DataStructures/Arrays/MinNoOfSwapsToSortArray.java
@@ -10,10 +10,10 @@ class MinNoOfSwapsToSortArray {
   */
   public static void main(String[] args) {
     int[] arr = {1, 5, 4, 3, 2};
-    System.out.println(getSwapCount(arr));
+    System.out.println(getSwapCount1(arr));
   }
 
-  static int getSwapCount(int[] arr){
+  static int getSwapCount1(int[] arr){
     int n=arr.length;
     boolean[] visited = new boolean[n];    //visited array
     Arrays.fill(visited, false);
@@ -41,4 +41,52 @@ class MinNoOfSwapsToSortArray {
     }
     return swaps;
   }
+  
+    public static int getSwapCount2(int[] arr)
+    {
+ 	int N = arr.length;
+        int ans = 0;
+        int[] temp = Arrays.copyOfRange(arr, 0, N);
+ 
+        // Hashmap which stores the
+        // indexes of the input array
+        HashMap<Integer, Integer> h
+            = new HashMap<Integer, Integer>();
+ 
+        Arrays.sort(temp);
+        for (int i = 0; i < N; i++)
+        {
+            h.put(arr[i], i);
+        }
+        for (int i = 0; i < N; i++)
+        {
+ 
+            // This is checking whether
+            // the current element is
+            // at the right place or not
+            if (arr[i] != temp[i])
+            {
+                ans++;
+                int init = arr[i];
+ 
+                // If not, swap this element
+                // with the index of the
+                // element which should come here
+                swap(arr, i, h.get(temp[i]));
+ 
+                // Update the indexes in
+                // the hashmap accordingly
+                h.put(init, h.get(temp[i]));
+                h.put(temp[i], i);
+            }
+        }
+        return ans;
+    }
+    
+    public static void swap(int[] arr, int i, int j)
+    {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
 }
