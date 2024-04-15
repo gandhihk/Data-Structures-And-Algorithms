1b258afe6c2c117ab49205fdeb47f50d8da49b08
Harsh Gandhi <harsh31.gandhi204@gmail.com>
2024-04-16 00:11:42 +0530

    Update and rename DetectCycleInGraphBFSAndDFS.java to DetectCycleInUnDirectedGraphBFSAndDFS.java

--- DataStructures/graphs/DetectCycleInUnDirectedGraphBFSAndDFS.java
+++ DataStructures/graphs/DetectCycleInUnDirectedGraphBFSAndDFS.java
@@ -4,7 +4,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
-public class DetectCycleInGraphBFSAndDFS {
+public class DetectCycleInUnDirectedGraphBFSAndDFS {
     static class Ele{
         int node, parent;
         Ele(int n, int p){
